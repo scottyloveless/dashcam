8721783c9e279e4ce1613f27908980f6ff16ea7d
scottyloveless <8176125+scottyloveless@users.noreply.github.com>
2026-03-02 13:27:09 -0600

    created some folders for client and collector

--- cmd/api/nodes.go
+++ cmd/api/nodes.go
@@ -3,21 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"net/netip"
 )
 
-type Location struct {
-	latitude  float32
-	longitude float32
-}
+// type Location struct {
+// 	latitude  float32
+// 	longitude float32
+// }
 
-type Node struct {
-	ipaddress   netip.Addr
-	location    Location
-	class       string
-	snmp        bool
-	snmpversion int
-}
+// type Node struct {
+// 	ipaddress   netip.Addr
+// 	location    Location
+// 	class       string
+// 	snmp        bool
+// 	snmpversion int
+// }
 
 func (app *application) createNodeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "create a new node")
