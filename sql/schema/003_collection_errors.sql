7e36d928c1c9aa5cb2a1933714be249ec4c9e62e
scottyloveless <8176125+scottyloveless@users.noreply.github.com>
2026-03-04 02:11:07 -0600

    003 mig complete

--- sql/schema/003_collection_errors.sql
+++ sql/schema/003_collection_errors.sql
@@ -1,16 +1,19 @@
 -- +goose Up
-CREATE TABLE metrics (
-	metric_name TEXT NOT NULL,
-	value FLOAT8 NOT NULL,
+CREATE TYPE error_type AS ENUM ('dns', 'unreachable', 'unauthorized', 'collector', 'saas_outage', 'isp');
+
+CREATE TABLE collection_errors (
 	device_id UUID NOT NULL,
 	requested_at TIMESTAMPTZ NOT NULL,
-	received_at TIMESTAMPTZ NOT NULL,
+	metric_name TEXT NOT NULL,
+	type error_type NOT NULL,
+	error_message TEXT NOT NULL,
+	retries INTEGER NOT NULL,
 	CONSTRAINT fk_deviceid FOREIGN KEY (device_id) REFERENCES devices(id),
 	PRIMARY KEY (requested_at, device_id, metric_name)
 );
 
 SELECT create_hypertable(
-	'metrics',
+	'collection_errors',
 	'requested_at',
 	partitioning_column => 'device_id',
 	number_partitions => 4,
@@ -18,4 +21,5 @@ SELECT create_hypertable(
 );
 
 -- +goose Down
-DROP TABLE metrics;
+DROP TABLE collection_errors;
+DROP TYPE error_type;
