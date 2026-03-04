135068c6d393cc2c82d28359c9fa179b48a9b4e3
scottyloveless <8176125+scottyloveless@users.noreply.github.com>
2026-03-04 01:33:07 -0600

    002 mig completed

--- sql/schema/002_metrics.sql
+++ sql/schema/002_metrics.sql
@@ -1,11 +1,18 @@
 -- +goose Up
 CREATE TABLE metrics (
-	name TEXT NOT NULL,
+	metric_name TEXT NOT NULL,
 	value FLOAT8 NOT NULL,
-	device_id UUID,
-	timestamp TIMESTAMPTZ NOT NULL,
-	answer_received TIMESTAMPTZ,
-	CONSTRAINT fk_deviceid FOREIGN KEY (device_id) REFERENCES devices(id)
+	device_id UUID NOT NULL,
+	collected_at TIMESTAMPTZ NOT NULL,
+	CONSTRAINT fk_deviceid FOREIGN KEY (device_id) REFERENCES devices(id),
+	PRIMARY KEY (collected_at, device_id, metric_name)
+);
+
+SELECT create_hypertable(
+	'metrics',
+	'collected_at',
+	partitioning_column => 'device_id',
+	chunk_time_interval => INTERVAL '7 days'
 );
 
 -- +goose Down
