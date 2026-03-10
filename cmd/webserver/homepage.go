bc2968a3b43056d09a8d3c35f246c6cb6659375f
scottyloveless <8176125+scottyloveless@users.noreply.github.com>
2026-03-10 17:53:01 -0500

    UI work, updated colorscheme, alerts on right panel

--- cmd/webserver/homepage.go
+++ cmd/webserver/homepage.go
@@ -14,15 +14,23 @@ type devicePingAndLocation struct {
 	RTTavg     float64
 }
 
+type homePageData struct {
+	Locations []string
+}
+
 func (app *application) homepageHandler(w http.ResponseWriter, r *http.Request) {
 	ctx := context.Background()
-	locations, err := app.queries.GetDistinctLocations(ctx)
+	locationStrSlice, err := app.queries.GetDistinctLocations(ctx)
 	if err != nil {
 		app.logger.Error(err.Error())
 		return
 	}
 
-	err = app.templates["home"].Execute(w, locations)
+	hpData := homePageData{
+		Locations: locationStrSlice,
+	}
+
+	err = app.templates["home"].Execute(w, hpData)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
