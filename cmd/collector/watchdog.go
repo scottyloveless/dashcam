7a7dfe511213a471e932191d9bb4dc753a1ec764
scottyloveless <8176125+scottyloveless@users.noreply.github.com>
2026-03-10 19:48:29 -0500

    started watchdog file

---
+++ cmd/collector/watchdog.go
@@ -0,0 +1,8 @@
+package main
+
+func (app *application) watchdog() {
+	// 1. ticker or sleep - i'm thinking ticker
+	// 2. fetch list of alerts
+	// 3. fetch last 5-10 metrics for that device and metric name, sort descending
+	// 4.
+}
