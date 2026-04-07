891483181bdf1f64049637e5a6f1656e9993ea2f
Scotty Loveless <scottyloveless@icloud.com>
2026-04-07 12:08:12 -0500

    added some SQL queries

--- sql/queries/auth.sql
+++ sql/queries/auth.sql
@@ -20,12 +20,12 @@ INSERT INTO sessions (
   id,
   user_id,
   created_at,
-  refresh_at,
+  refreshed_at,
   expires_at,
   token_hash
 )
 VALUES (
-  uuid(),
+  gen_random_uuid(),
   $1,
   now(),
   now(),
@@ -47,3 +47,30 @@ LIMIT 1;
 -- name: DeleteSession :exec
 DELETE FROM sessions
 WHERE token_hash = $1;
+
+-- name: UpdateLastLogin :exec
+UPDATE users
+SET last_login = NOW()
+WHERE id = $1;
+
+-- name: CreateUser :exec
+INSERT INTO users (
+    id,
+    first_name,
+    last_name,
+    role,
+    job_title,
+    email,
+    phone_number,
+    password_hash
+)
+VALUES (
+    $1,
+    $2,
+    $3,
+    $4,
+    $5,
+    $6,
+    $7,
+    $8
+);
