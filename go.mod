3ea17e317de8f4a254885d837d27bc919cdb5eff
Scotty Loveless <8176125+scottyloveless@users.noreply.github.com>
2026-04-22 21:23:11 -0500

    ninja oauth2 support added

--- go.mod
+++ go.mod
@@ -6,13 +6,12 @@ require (
 	github.com/google/uuid v1.6.0
 	github.com/jackc/pgx/v5 v5.8.0
 	github.com/joho/godotenv v1.5.1
-	github.com/julienschmidt/httprouter v1.3.0
 	github.com/prometheus-community/pro-bing v0.8.0
 	golang.org/x/crypto v0.48.0
+	golang.org/x/oauth2 v0.36.0
 )
 
 require (
-	github.com/go-echarts/go-echarts/v2 v2.7.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
---
+++ internal/integrations/ninja/oauth2.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/clientcredentials"
+)
+
+type Client struct {
+	httpClient *http.Client
+	baseURL    string
+}
+
+func NewClient(ctx context.Context) (*Client, error) {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	clientID := os.Getenv("NINJA_CLIENT_ID")
+	clientSecret := os.Getenv("NINJA_CLIENT_SECRET")
+	tokenURL := os.Getenv("NINJA_TOKEN_URL")
+	baseURL := os.Getenv("NINJA_INSTANCE_URL")
+
+	if clientID == "" || clientSecret == "" || tokenURL == "" || baseURL == "" {
+		return nil, fmt.Errorf("missing required NINJA_* environment variables")
+	}
+
+	cfg := &clientcredentials.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		TokenURL:     tokenURL,
+		Scopes:       []string{"monitoring"},
+	}
+
+	tokenFetchClient := &http.Client{Timeout: 10 * time.Second}
+	ctx = context.WithValue(ctx, oauth2.HTTPClient, tokenFetchClient)
+
+	return &Client{
+		httpClient: cfg.Client(ctx),
+		baseURL:    baseURL,
+	}, nil
+}
+
+func main() {
+	c, err := NewClient(context.Background())
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	resp, err := c.httpClient.Get(c.baseURL + "/v2/alerts")
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Print(string(data))
+}
